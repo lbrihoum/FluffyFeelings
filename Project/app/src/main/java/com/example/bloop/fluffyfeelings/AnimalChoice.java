174e9ee5a25b20ffe5ab13e3ce627d559460ac8d
Lina Yara Brihoum <brihoumlina@yahoo.com>
2019-03-02 09:06:41 -0500

    Creating second screen and being able to navigate to it

---
+++ Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalChoice.java
@@ -0,0 +1,13 @@
+package com.example.bloop.fluffyfeelings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnimalChoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_choice);
+    }
+}
--- Project/app/src/main/java/com/example/bloop/fluffyfeelings/Login.java
+++ Project/app/src/main/java/com/example/bloop/fluffyfeelings/Login.java
@@ -1,7 +1,10 @@
 package com.example.bloop.fluffyfeelings;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        addButtonClickEventListener();
+    }
+
+    public void addButtonClickEventListener() {
+        Button button = (Button) findViewById(R.id.loginButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, AnimalChoice.class);
+                startActivity(intent);
+            }
+        });
     }
 }
