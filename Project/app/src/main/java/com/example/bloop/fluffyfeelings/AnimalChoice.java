af3a1eed69b3df467929f4efc1cb3b24c2f5f32f
Lina Yara Brihoum <brihoumlina@yahoo.com>
2019-03-03 10:31:35 -0500

    Creating third screen & connecting it to others

--- Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalChoice.java
+++ Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalChoice.java
@@ -1,7 +1,11 @@
 package com.example.bloop.fluffyfeelings;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class AnimalChoice extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class AnimalChoice extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal_choice);
+        addButtonClickEventListener();
+
+    }
+
+    public void addButtonClickEventListener() {
+        Button button = (Button) findViewById(R.id.goButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AnimalChoice.this, AnimalView.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalView.java
@@ -0,0 +1,14 @@
+package com.example.bloop.fluffyfeelings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class AnimalView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_view);
+    }
+}
