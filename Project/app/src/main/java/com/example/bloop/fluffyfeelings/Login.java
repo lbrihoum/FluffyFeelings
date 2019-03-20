cc5e19ca3c45951c4d5e15915f5083d2a56211ec
Lina Yara Brihoum <brihoumlina@yahoo.com>
2019-03-20 17:22:22 -0400

    Beginning to add login functionality

--- Project/app/src/main/java/com/example/bloop/fluffyfeelings/Login.java
+++ Project/app/src/main/java/com/example/bloop/fluffyfeelings/Login.java
@@ -1,17 +1,34 @@
 package com.example.bloop.fluffyfeelings;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private Button loginBtn;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
 
         addButtonClickEventListener();
     }
