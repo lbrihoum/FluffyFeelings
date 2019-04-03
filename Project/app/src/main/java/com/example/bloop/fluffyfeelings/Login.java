d913e813f96e8c1b3bc6b12d5b982cc79f13b8f8
Lina Yara Brihoum <brihoumlina@yahoo.com>
2019-04-03 16:25:03 -0400

    Starting to implement Google API Login

--- Project/app/src/main/java/com/example/bloop/fluffyfeelings/Login.java
+++ Project/app/src/main/java/com/example/bloop/fluffyfeelings/Login.java
@@ -11,23 +11,37 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity {//implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
     private EditText inputEmail, inputPassword;
-    private FirebaseAuth auth;
     private Button loginBtn;
     private ProgressDialog progressDialog;
+
+    private FirebaseAuth auth;
+
+    GoogleApiClient googleApiClient;
+
+    private static final String TAG = "SignInActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestEmail()
+//                .build();
+//        googleApiClient = new GoogleApiClient.Builder(this)
+//                .enableAutoManage(this, this)
+//                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+//                .build();
 
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Logging In...");
@@ -40,6 +54,31 @@ public class Login extends AppCompatActivity {
 
         addButtonClickEventListener();
     }
+
+    private void signIn() {
+        //Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        //startActivityForResult(intent, 9001);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the intent
+        if (requestCode == 9001) {
+            //GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            //handleSignInResult(result);
+        }
+    }
+
+//    private void handleSignInResult(GoogleSignInResult result) {
+//        Log.d(TAG, "handleSignInResult: " + result.isSuccess());
+//        if (result.isSuccess()) {
+//            // Signed In Successfully
+//            GoogleSignInAccount account = result.getSignInAccount();
+//
+//        }
+//    }
 
     public void addButtonClickEventListener() {
         loginBtn.setOnClickListener(new View.OnClickListener() {
