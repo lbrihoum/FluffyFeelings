8f41a3328a1baa86effdf69c56033a44c1963c35
zoe <zsewell11@gmail.com>
2019-04-03 17:52:42 -0400

    changed the like button and added dog image.
    tried to implement like button thing but it wasnt working

--- Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalView.java
+++ Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalView.java
@@ -1,8 +1,11 @@
 package com.example.bloop.fluffyfeelings;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class AnimalView extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class AnimalView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal_view);
+        
     }
 }
