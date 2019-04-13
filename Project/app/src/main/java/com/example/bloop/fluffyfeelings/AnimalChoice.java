2c366836c3ff3ec801e97fbc77954c00694e3297
sakul360 <40379198+sakul360@users.noreply.github.com>
2019-04-12 22:10:15 -0400

    data added to intent to pass to animal view page from animal choice page

--- Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalChoice.java
+++ Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalChoice.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class AnimalChoice extends AppCompatActivity {
 
@@ -13,16 +14,41 @@ public class AnimalChoice extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal_choice);
+
         addButtonClickEventListener();
 
     }
+    public void onCheckboxClicked (View view)
+    {
+        boolean checked = ((CheckBox) view).isChecked();
+        Intent intent2 = new Intent(AnimalChoice.this, AnimalView.class);
+        switch (view.getId())
+        {
+            case R.id.checkBox1:
+                if (checked)
+                    intent2.putExtra("cats", true);
+            case R.id.checkBox2:
+                if (checked)
+                    intent2.putExtra("dogs", true);
+            case R.id.checkBox3:
+                if (checked)
+                    intent2.putExtra("birds",true);
+            case R.id.checkBox4:
+                if (checked)
+                    intent2.putExtra("bunnies",true);
+            case R.id.checkBox5:
+                if (checked)
+                    intent2.putExtra("lizards",true);
 
+        }
+    }
     public void addButtonClickEventListener() {
         Button button = (Button) findViewById(R.id.goButton);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 Intent intent = new Intent(AnimalChoice.this, AnimalView.class);
                 startActivity(intent);
             }
--- Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalView.java
+++ Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalView.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.content.Intent;
 
 public class AnimalView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //boolean cats = getIntent().getExtras().getBoolean("cats");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal_view);
         
     }
+
 }
