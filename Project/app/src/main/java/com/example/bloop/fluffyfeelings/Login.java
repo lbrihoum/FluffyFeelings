5a798a9bd7a68ecbb6e869216b3c4d16d9c43a1d
Lina Yara Brihoum <brihoumlina@yahoo.com>
2019-04-12 20:19:06 -0400

    Implementing google sign in api

--- Project/app/src/main/java/com/example/bloop/fluffyfeelings/Login.java
+++ Project/app/src/main/java/com/example/bloop/fluffyfeelings/Login.java
@@ -11,12 +11,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Login extends AppCompatActivity {//implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
@@ -27,6 +36,11 @@ public class Login extends AppCompatActivity {//implements GoogleApiClient.OnCon
     private FirebaseAuth auth;
 
     GoogleApiClient googleApiClient;
+    SignInButton googleButton; // Google sign in button
+
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private final static int RC_SIGN_IN = 9001;
 
     private static final String TAG = "SignInActivity";
 
@@ -35,89 +49,104 @@ public class Login extends AppCompatActivity {//implements GoogleApiClient.OnCon
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                .requestEmail()
-//                .build();
-//        googleApiClient = new GoogleApiClient.Builder(this)
-//                .enableAutoManage(this, this)
-//                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-//                .build();
-
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Logging In...");
 
+        inputEmail = findViewById(R.id.email);
+        inputPassword = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginButton);
+        googleButton = findViewById(R.id.googleSignIn);
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         auth = FirebaseAuth.getInstance();
 
-        inputEmail = (EditText) findViewById(R.id.email);
-        inputPassword = (EditText) findViewById(R.id.password);
-        loginBtn = (Button) findViewById(R.id.loginButton);
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
 
         addButtonClickEventListener();
     }
 
     private void signIn() {
-        //Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-        //startActivityForResult(intent, 9001);
+        signOut();
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+
+        //mGoogleSignInClient.signOut().addOnCompleteListener(this)
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        auth.signOut();
+
+        // Google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //updateUI(null);
+                    }
+                });
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the intent
-        if (requestCode == 9001) {
-            //GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            //handleSignInResult(result);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                //updateUI(null);
+            }
         }
     }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        auth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "signInWithCredential:Success");
+                    FirebaseUser user = auth.getCurrentUser();
+                    //updateUI(user);
+                } else {
+                    Log.w(TAG, "SignInWithCredential:Failure", task.getException());
+                    Toast.makeText(Login.this, "Authentication Failed", Toast.LENGTH_LONG).show();
+                    //updateUI(null);
+                }
+            }
+        });
+    }
 
-//    private void handleSignInResult(GoogleSignInResult result) {
-//        Log.d(TAG, "handleSignInResult: " + result.isSuccess());
-//        if (result.isSuccess()) {
-//            // Signed In Successfully
-//            GoogleSignInAccount account = result.getSignInAccount();
-//
-//        }
-//    }
 
     public void addButtonClickEventListener() {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String email = inputEmail.getText().toString();
-                final String password = inputPassword.getText().toString();
-                if (password.length() == 0 || email.length() == 0) {
-                    Toast.makeText(Login.this, "Fill All Fields", Toast.LENGTH_LONG).show();
-                } else {
-                    progressDialog.show();
-                    Intent intent = new Intent(Login.this, AnimalChoice.class);
-                    startActivity(intent);
-                    progressDialog.dismiss();
-                }
-//                try {
-//                    if (password.length() != 0 || email.length() != 0) {
-//                        progressDialog.show();
-//                        auth.signInWithEmailAndPassword(email, password)
-//                                .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
-//                                    @Override
-//                                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                                        if (!task.isSuccessful()) {
-//                                            Toast.makeText(Login.this, "Authentication Failed", Toast.LENGTH_LONG).show();
-//                                            Log.v("error", task.getResult().toString());
-//                                        } else {
-//                                            Intent intent = new Intent(Login.this, AnimalChoice.class);
-//                                            startActivity(intent);
-//                                            progressDialog.dismiss();
-//                                        }
-//                                        progressDialog.dismiss();
-//                                    }
-//                                });
-//                    } else {
-//                        Toast.makeText(Login.this, "Fill All Fields", Toast.LENGTH_LONG).show();
-//                    }
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-            }
+                progressDialog.show();
+                Intent intent = new Intent(Login.this, AnimalChoice.class);
+                startActivity(intent);
+                progressDialog.dismiss();
+        }
         });
     }
 
