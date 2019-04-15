f295d03525103a5cd53d9a7d997009ecb76da87d
sakul360 <40379198+sakul360@users.noreply.github.com>
2019-04-14 20:22:26 -0400

    code for passing data from animal choice screen to animal view screen is good, the getExtra commands just keep throwing null pointer exceptions

--- Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalChoice.java
+++ Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalChoice.java
@@ -54,4 +54,6 @@ public class AnimalChoice extends AppCompatActivity {
             }
         });
     }
+
 }
+
--- Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalView.java
+++ Project/app/src/main/java/com/example/bloop/fluffyfeelings/AnimalView.java
@@ -12,7 +12,23 @@ public class AnimalView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //boolean cats = getIntent().getExtras().getBoolean("cats");
+
+        Bundle bundle = getIntent().getExtras();
+        boolean cats = bundle.getBoolean("cats",true);
+        boolean dogs = bundle.getBoolean("dogs",true);
+        boolean birds = bundle.getBoolean("birds", true);
+        boolean bunnies = bundle.getBoolean("bunnies", true);
+        boolean lizards = bundle.getBoolean("lizards", true);
+        if(cats)
+            //insert code for pulling cat pics from the firebase
+        if(dogs)
+            //insert code for pulling dog pics from the firebase
+        if (birds)
+            //insert code for pulling bird pics from the firebase
+        if (bunnies)
+            //insert code for pulling bunny pics from the firebase
+        if (lizards)
+            //insert code for pulling lizard pics from the firebase
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal_view);
         
